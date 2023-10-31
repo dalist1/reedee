4a39d881e22af90663def6c0629ef1a8a3c82531
Frensi <52313111+dalist1@users.noreply.github.com>
2023-10-31 18:25:34 +0000

    new sw

--- app/500.tsx
+++
@@ -1,7 +0,0 @@
-export default function ServerError() {
-    return (
-        <>
-            <h1>500 page, server side error</h1>
-        </>
-    )
-}
--- app/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function Error() {
-    return (
-        <>
-            <h1>Error page</h1>
-        </>
-    )
-}
