f0c54ceb850aff4f68c9e5bf71341bb69eb837c2
Frensi <52313111+dalist1@users.noreply.github.com>
2023-12-04 17:13:33 +0000

    ripple & update deps. TODO: exclude switch

--- components/settings/OptionsList.tsx
+++ components/settings/OptionsList.tsx
@@ -1,6 +1,8 @@
 import { FC, useCallback } from 'react';
 import { Switch } from '@/components/ui/switch'
 import ToggleButton from '@/components/settings/ToggleButtons';
+import { useRipple } from './use-ripple';
+import Ripple from './ripple';
 
 type Option = {
   name: string;
@@ -20,8 +22,43 @@ type OptionsListProps = {
   mainIndex: number;
 };
 
+const ButtonWrapper: FC<{ option: Option, index: number, handleCheckedChange: (index: number) => void }> = ({ option, index, handleCheckedChange }) => {
+  const { ripples, onClick, onClear } = useRipple();
+
+  return (
+    <div
+      key={index}
+      className="select-none relative z-0 flex w-full justify-between items-center hover:bg-slate-500/20 p-4 px-4 rounded-2xl cursor-pointer overflow-hidden"
+      onClick={option.component === 'toggle' ? undefined : (event) => {
+        handleCheckedChange(index);
+        onClick(event);
+      }}
+    >
+      <Ripple ripples={ripples} onClear={onClear} className='absolute inset-0' />
+      <div className="bg-slate-950 p-4 justify-start items-center rounded-full">
+        <span className="flex justify-start text-white cursor-pointer bg-slate-950 hover:bg-slate-900 rounded-full">
+          {option.icon}
+        </span>
+      </div>
+      <span>{option.name}</span>
+      <span>
+        {option.component === 'switch' &&
+          <div className='pointer-events-none'>
+            <Switch
+              id={`switch-${index}`}
+              checked={option.checked}
+              onCheckedChange={() => handleCheckedChange(index)}
+              className='pointer-events-none'
+            />
+          </div>}
+        {option.component === 'toggle' && <ToggleButton />}
+      </span>
+    </div>
+  );
+};
+
 const OptionsList: FC<OptionsListProps> = ({ options, handleSwitchChange, mainIndex }) => {
-  const handleCheckedChange = useCallback((index) => {
+  const handleCheckedChange = useCallback((index: number) => {
     handleSwitchChange(mainIndex, index);
     options[index].handler();
   }, [options, handleSwitchChange, mainIndex]);
@@ -29,26 +66,7 @@ const OptionsList: FC<OptionsListProps> = ({ options, handleSwitchChange, mainIn
   return (
     <div className='flex flex-col justify-between items-center w-full'>
       {options.map((option, index) => (
-        <div
-          key={index}
-          className="flex w-full justify-between items-center hover:bg-slate-500/20 p-4 px-4 rounded-2xl cursor-pointer"
-        >
-          <div className="bg-slate-950 p-4 justify-start items-center rounded-full">
-            <span className="flex justify-start text-white cursor-pointer bg-slate-950 hover:bg-slate-900 rounded-full">
-              {option.icon}
-            </span>
-          </div>
-          <span>{option.name}</span>
-          <span>
-            {option.component === 'switch' &&
-              <Switch
-                id={`switch-${index}`}
-                checked={option.checked}
-                onCheckedChange={() => handleCheckedChange(index)}
-              />}
-            {option.component === 'toggle' && <ToggleButton />}
-          </span>
-        </div >
+        <ButtonWrapper key={index} option={option} index={index} handleCheckedChange={handleCheckedChange} />
       ))}
     </div>
   );
--- components/settings/ToggleButtons.tsx
+++ components/settings/ToggleButtons.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { useRipple } from './use-ripple';
+import Ripple from './ripple';
 import { TbMultiplier1X, TbMultiplier15X, TbMultiplier2X } from "react-icons/tb";
-import { ReactNode } from 'react'
+import { ReactNode } from 'react';
 import { useTextStore } from '@/stores/useTextStore';
 
 type toggleType = {
@@ -20,28 +22,35 @@ const toggleConfig: toggleType[] = [
     },
 ];
 
-const Button = () => {
-    const { selectedSize, setSelectedSize } = useTextStore()
+const Button = ({ index }) => {
+    const { selectedSize, setSelectedSize } = useTextStore();
+    const { ripples, onClick: rippleOnClick, onClear } = useRipple();
+    const ic = toggleConfig[index];
 
-    const handleButtonClick = (buttonId: number) => {
-        setSelectedSize(buttonId);
+    const isActive = selectedSize === index;
+
+    const handleButtonClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        setSelectedSize(index);
+        rippleOnClick(event);
     }
-    return toggleConfig.map((ic, index) => {
-        const isActive = selectedSize === index;
-        return (
-            <button
-                className={`hover:text-white p-2.5 text-gray-300 rounded-xl ${isActive ? 'bg-blue-500 text-white' : 'bg-slate-950/30 text-slate-300'}`}
-                onClick={() => handleButtonClick(index)}>
-                {ic.icon}
-            </button>
-        );
-    });
+
+    return (
+        <button
+            className={`relative overflow-hidden hover:text-white p-2.5 text-gray-300 rounded-xl ${isActive ? 'bg-blue-500 text-white' : 'bg-slate-950/30 text-slate-300'}`}
+            onClick={handleButtonClick}>
+            {ic.icon}
+            <Ripple ripples={ripples} onClear={onClear} className="absolute inset-0" />
+        </button>
+    );
 }
 
 export default function ToggleButton() {
     return (
         <div className="flex gap-x-2">
-            <Button />
+            {toggleConfig.map((_, index) => (
+                <Button key={index} index={index} />
+            ))}
         </div>
-    )
+    );
 }
+
