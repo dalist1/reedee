5c69bf286b9aed734079d3ad77909a371633e4ec
Frensi <52313111+dalist1@users.noreply.github.com>
2023-12-05 21:26:30 +0000

    more adjustments

--- components/reading/Controls.tsx
+++ components/reading/Controls.tsx
@@ -27,14 +27,14 @@ export default function Controls({ goToNextPage, goToPreviousPage, currentPageTe
     };
 
     return (
-        <div className="z-50 flex h-[4.5rem] justify-between items-center w-[22rem] md:w-96 fixed mx-3 font-medium text-sm bottom-4 p-4 bg-blue-950/80 backdrop-blur-sm rounded-full">
+        <div className="z-50 flex h-[4.5rem] justify-between items-center w-[22rem] md:w-96 fixed mx-3 font-medium text-sm bottom-4 p-4 bg-slate-950/80 backdrop-blur-sm rounded-full">
             <span className="flex justify-center items-center p-4 cursor-pointer">
                 <AnimatedLike fileName={fileName} size={24} color="#6b7280" />
             </span>
             <button className='flex cursor-pointer justify-items items-center p-3 text-gray-500 hover:text-white rounded-full' onClick={() => goToPreviousPage()}>
                 <TbArrowBackUp size={30} />
             </button>
-            <button className='relative p-3 cursor-pointer shadow-sm bg-slate-950/30 hover:bg-slate-950/40 text-white rounded-full overflow-hidden'
+            <button className='relative p-3 cursor-pointer shadow-sm bg-blue-900/50 hover:bg-blue-900/70 text-white rounded-full overflow-hidden'
                 onClick={(event) => { toggleAudio(); onClick(event) }}>
                 {isPlaying ? <FaPause size={26} /> : <FaPlay size={26} />}
                 <Ripple ripples={ripples} onClear={onClear} className='absolute inset-0' />
