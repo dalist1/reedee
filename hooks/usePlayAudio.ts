9d22e301fbac8398e609dc527f07e31f2eb62b37
Frensi <52313111+dalist1@users.noreply.github.com>
2023-12-26 12:58:06 +0000

    audio v2, add prefetching for smooth playback, remove compromise lib

--- hooks/usePlayAudio.ts
+++ hooks/usePlayAudio.ts
@@ -1,17 +1,18 @@
 import { useState, useCallback, useEffect, useRef } from 'react';
 import { useNavigationStore } from '@/stores/useNavigationStore';
-import nlp from 'compromise';
 
 export const usePlayAudio = (currentPageText: string, numPages?: number, currentPage?: number) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const sentences = useRef(nlp(currentPageText).sentences().out('array'));
+  const regex = /(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/gm;
+  const sentences = useRef(currentPageText.split(regex));
   const currentSentenceIndex = useRef(0);
   const audioElementRef = useRef<HTMLAudioElement>(new Audio());
+  const audioQueue = useRef([]);
 
   const { goToNextPage } = useNavigationStore();
 
-  const fetchAudio = useCallback((text) => {
+  const fetchAudio = useCallback((text: string) => {
     setIsLoading(true);
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
@@ -19,7 +20,7 @@ export const usePlayAudio = (currentPageText: string, numPages?: number, current
       xhr.open('GET', `https://gttsfastapi.vercel.app/tts/en?${params}`);
       xhr.responseType = 'blob';
       xhr.onload = () => {
-        setIsLoading(false); // Audio finishes loading
+        setIsLoading(false);
         if (xhr.status === 200) {
           resolve(URL.createObjectURL(xhr.response));
         } else {
@@ -34,13 +35,46 @@ export const usePlayAudio = (currentPageText: string, numPages?: number, current
     });
   }, []);
 
+  const prefetchAudio = useCallback((text: string) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      const params = new URLSearchParams({ text });
+      xhr.open('GET', `https://gttsfastapi.vercel.app/tts/en?${params}`);
+      xhr.responseType = 'blob';
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          resolve(URL.createObjectURL(xhr.response));
+        } else {
+          reject(new Error('Fetching audio failed with status: ' + xhr.status));
+        }
+      };
+      xhr.onerror = () => {
+        reject(new Error('Network error while fetching audio'));
+      };
+      xhr.send();
+    });
+  }, []);
+
   const playSentence = useCallback(
-    async (index) => {
+    async (index: number) => {
       setIsPlaying(true);
       currentSentenceIndex.current = index;
-      const audioSrc = await fetchAudio(sentences.current[index]);
+
+      let audioSrc;
+      if (audioQueue.current.length > 0) {
+        audioSrc = audioQueue.current.shift();
+      } else {
+        const sentence = sentences.current[index];
+        audioSrc = await fetchAudio(sentence);
+      }
       audioElementRef.current.src = audioSrc;
-      audioElementRef.current.play().then(() => setIsLoading(false)); // Set isLoading to false when audio starts playing
+      audioElementRef.current.play().then(() => setIsLoading(false));
+
+      if (index + 1 < sentences.current.length) {
+        const nextSentence = sentences.current[index + 1];
+        const nextAudioSrc = await prefetchAudio(nextSentence);
+        audioQueue.current.push(nextAudioSrc);
+      }
 
       audioElementRef.current.onended = () => {
         if (index + 1 < sentences.current.length) {
@@ -54,7 +88,7 @@ export const usePlayAudio = (currentPageText: string, numPages?: number, current
         }
       };
     },
-    [fetchAudio, numPages, currentPage, goToNextPage],
+    [fetchAudio, numPages, currentPage, goToNextPage, prefetchAudio],
   );
 
   const togglePlayPause = useCallback(() => {
@@ -81,13 +115,18 @@ export const usePlayAudio = (currentPageText: string, numPages?: number, current
     }
     setIsPlaying(false);
     currentSentenceIndex.current = 0;
+    audioQueue.current = [];
   }, []);
 
   useEffect(() => {
     cleanup();
-    sentences.current = nlp(currentPageText).sentences().out('array') || [];
+    sentences.current = currentPageText.split(regex) || [];
     if (isPlaying) {
-      playSentence(0);
+      const firstSentence = sentences.current[0];
+      prefetchAudio(firstSentence).then((firstAudioSrc) => {
+        audioQueue.current.push(firstAudioSrc);
+        playSentence(0);
+      });
     }
   }, [currentPageText, cleanup]);
 
